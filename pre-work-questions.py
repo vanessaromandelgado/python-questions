11717ffe30448a720f221ead0c23bda1be5a0fde
vanessaromandelgado <vanessa.roman.delgado@gmail.com>
2021-11-19 06:29:09 -0600

    First commit

---
+++ pre-work-questions.py
@@ -0,0 +1,35 @@
+#Q 1
+def hello_name(user_name):
+    print("hello_USERNAME!")
+
+#Q2, Prints odd numbers from 1-100, and returns nothing
+def first_odds():
+    while count!=100:
+    odd_numbers=():  
+    print(first_odds)
+
+
+#Q3 fuction to return the max number of given list
+def max_num_in_list(a_list):
+    max list(100):
+    min list(0:)
+    print(max_num_in_list)
+    
+
+#Q4 Retrun if a given year is a leap year, 
+# leap year is divisible by 4, but not divisible by 100, 
+# unless it is also divisible by 400.
+def is_leap_year(a_year):
+    def leap_year(n):
+    if n%4=0 and n%100!=):
+    return year % 100!=0
+    year%400=
+    print(is_leap_year)
+
+#Q5 Write a function to check if all numbers in a list
+# are consecutive numbers
+# return should be boolean type
+def is_consecutive(a_list):
+    maximum=max(1):
+    if sum(1): maximum + maximum+1) /2
+    Return: True
